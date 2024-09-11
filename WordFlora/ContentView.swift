fe3effef7130c188e35600f6feed9eb0f1fc7378
wolfayet0855 <online_alt02@yahoo.com>
2024-09-11 15:50:42 -0400

    Initial Commit

---
+++ WordFlora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordFlora
+//
+//  Created by user on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordFlora/WordFloraApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFloraApp.swift
+//  WordFlora
+//
+//  Created by user on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordFloraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
